601f948a29c5c201c2f32f57ddf506ce926eb8f2
MinkNhat <nquyenminhnhat0503@gmail.com>
2025-10-02 16:09:25 +0700

    add validation to category, product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
+# custom validator
+class ProductNameStartWithSpValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value.start_with?("SP_")
+      record.errors.add(attribute, "Ten san pham phai bat dau bang SP_")
+    end
+  end
+end
+
+
 class Product < ApplicationRecord
   belongs_to :category
+  validates :name, :price, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :price, numericality: true, comparison: { greater_than: 0 }
+  validates :name, product_name_start_with_sp: true
 end
