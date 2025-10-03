ceb2768f8aae2cfe93099c67e1742439c7e45c9a
MinkNhat <nquyenminhnhat0503@gmail.com>
2025-10-03 15:30:42 +0700

    add model User, User serializer

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+  enum role: { user: 0, admin: 1 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
+end
