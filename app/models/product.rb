d4a346ef816f1520436b32f639f5f351c72d477f
MinkNhat <nquyenminhnhat0503@gmail.com>
2025-10-10 15:00:41 +0700

    update igrone blank properties in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,15 +11,18 @@ end
 class Product < ApplicationRecord
   belongs_to :category
   has_many :product_images, -> { order(:position) }, dependent: :destroy # -> { order(:position) } scope sắp xếp position
-  has_many :product_properties, dependent: :destroy
+  has_many :product_properties, dependent: :destroy, reject_if: :reject_properties
 
   # nested attributes --> cho phép tạo / sửa model con qua model cha
   accepts_nested_attributes_for :product_properties, allow_destroy: true
   accepts_nested_attributes_for :product_images, allow_destroy: true
 
-
   validates :name, :price, presence: true
   validates :name, length: { maximum: 50 }
   validates :price, numericality: true, comparison: { greater_than: 0 }
   validates :name, product_name_start_with_sp: true
+
+  def reject_properties(property)
+    property[:key].blank?
+  end
 end
