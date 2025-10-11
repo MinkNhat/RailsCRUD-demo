0cf93677b0850b62006598991e3c0688a2f49b64
MinkNhat <nquyenminhnhat0503@gmail.com>
2025-10-11 10:43:23 +0700

    fix N+1 in user get all products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
    class ProductsController < ApplicationController
       def index
-        products = Product.all.includes(:category, product_images: { image_attachment: :blob })
+        products = Product.all.includes(:category, product_properties: [], images_attachments: :blob)
         render json: products, each_serializer: ProductSerializer
       end
 
